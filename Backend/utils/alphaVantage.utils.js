e24f2c2ccc9d376ccf87989e5abd55dcfd5aea7f
Zilean12 <aryan.sharma21@st.niituniversity.in>
2024-05-24 14:07:16 +0530

    stock api

--- Backend/utils/alphaVantage.utils.js
+++ Backend/utils/alphaVantage.utils.js
@@ -1,13 +1,11 @@
 const axios = require('axios');
-const dotenv = require('dotenv');
 
-dotenv.config();
+const alphaVantageApiKey = process.env.ALPHA_VANTAGE_API_KEY;
 
+// Function to fetch stock data from Alpha Vantage API
 const fetchStockData = async (stockSymbol) => {
-  // const apiKey = process.env.ALPHA_VANTAGE_API_KEY;
-  const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo`;
-
   try {
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${stockSymbol}&interval=5min&apikey=${alphaVantageApiKey}`;
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
--- Frontend/src/component/Stocks/StockDashboard.tsx
+++ Frontend/src/component/Stocks/StockDashboard.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import './StockDashboard.css';
 
 import AuthContext from '../../contexts/AuthContext';
 
